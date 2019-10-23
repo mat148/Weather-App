9b326ace9d251caeeef7b74de0015c631a63c519
Mathew Morse <mrm1779@gmail.com>
2019-10-23 14:27:35 -0400

    setting up enviroment variables

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express()
 
 app.use(express.static('public'));
@@ -11,10 +12,14 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = 'api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}'
+    
     res.render('index');
-    console.log(req.body.city);
+    //console.log(req.body.city);
+    console.log(value);
 })
 
 app.listen(3000, function() {
-    console.log('Example app listening on port 3000')
+    console.log('Example app listening on port 3000');
 })
