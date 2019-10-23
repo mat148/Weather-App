bc9f568ed8dcdbd5171b2358bbb51c46e8de43ea
Mathew Morse <mrm1779@gmail.com>
2019-10-23 15:31:54 -0400

    sucessfully take user input, make a call to the openWeatherAPI, and post the results on the front-end

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
-console.log('No value for FOO yet:', process.env.weatherApiKey);
 
 const app = express()
 
@@ -13,16 +13,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
     let city = req.body.city;
     let apiKey = process.env.weatherApiKey;
-    let url = 'api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}'
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-    res.render('index');
-    console.log(url);
+    request(url, function(err, response, body) {
+        if(err) {
+            res.render('index', {weather: null, error: url});
+        }else {
+            let weather = JSON.parse(body)
+
+            if (weather.main == undefined) {
+                res.render('index', {weather: null, error: 'Error 2'});
+            }else {
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('index', {weather: weatherText, error: null});
+            }
+        }
+    });
+
+    //res.render('index');
+    //console.log(url);
 })
 
 app.listen(3000, function() {
